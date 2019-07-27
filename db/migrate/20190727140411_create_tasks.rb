d97036eb85b23e92f4e4c7259b3cb191cfbf8a13
Yusaku ONO <yono.05@gmail.com>
2019-07-27 23:19:03 +0900

    Add Task model

---
+++ db/migrate/20190727140411_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string :title, null: false
+      t.datetime :done_at
+
+      t.timestamps
+    end
+  end
+end
